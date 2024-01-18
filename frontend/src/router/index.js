aa14ab628fdc54338953902c481a409bf9100f10
unknown <tlswnd3127@gmail.com>
2024-01-18 11:52:22 +0900

    Feat openVidu call-react & fix MUI version error

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,11 +6,14 @@ import Lobby from "../pages/Lobby";
 import Login from "../pages/Auth/Login";
 import Signup from "../pages/Auth/Signup";
 import Layout from "../layout";
+import VideoRoomComponent from "../components/OpenVidu/VideoRoomComponent";
+
 const route = (
     <>
     <Route path="/" element={<Start />}></Route>
     <Route element={<Layout/>}>
         <Route path="room" element={<Room />}></Route>
+        <Route path="room2" element={<VideoRoomComponent />}></Route>
         <Route path="lobby" element={<Lobby />}></Route>
         <Route path="auth">
             <Route path="login" element={<Login />}></Route>
