2eef767e9b573e54fe3d94d5067ac7110e60bf51
Shinhyeonjung <tlswnd3127@gmail.com>
2024-01-18 17:05:04 +0900

    Rename router, Layout

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,18 +1,17 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 
-import Start from "../pages/Start";
-import Room from "../pages/Room";
-import Lobby from "../pages/Lobby";
-import Login from "../pages/Auth/Login";
-import Signup from "../pages/Auth/Signup";
-import Layout from "../layout";
-import VideoRoomComponent from "../components/OpenVidu/VideoRoomComponent";
-
+import Start from "./pages/Start";
+import Room from "./pages/Room";
+import Lobby from "./pages/Lobby";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Layout from "./components/layout";
+import VideoRoomComponent from "./components/OpenVidu/VideoRoomComponent";
 const route = (
     <>
     <Route path="/" element={<Start />}></Route>
     <Route element={<Layout/>}>
-        {/* <Route path="room" element={<Room />}></Route> */}
+        <Route path="room" element={<Room />}></Route>
         <Route path="room2" element={<VideoRoomComponent />}></Route>
         <Route path="lobby" element={<Lobby />}></Route>
         <Route path="auth">
