a2753191125ed4d803b3a872b538473ba9192183
wony <wondreaming@naver.com>
2024-01-18 17:36:22 +0900

    Fix

--- frontend/src/pages/Start/index.js
+++ frontend/src/pages/Start/index.js
@@ -8,11 +8,11 @@ function Start() {
         <h2 className="font-['pixel']">abc 가나다</h2> */}
       <img className="w-4/5 max-w-2xl
       animate-jump-in animate-duration-[2000ms] z-10" 
-      alt="Logo" src="img/logo.png" />
+      alt="Logo" src="image/logo.png" />
 
       <img className="animate-fade animate-delay-[2000ms] 
         absolute h-screen w-full" 
-        alt="Background" src="img/startBackground.png"/>
+        alt="Background" src="image/startBackground.png"/>
       {/* <div className='z-10 absolute w-3/4 h-3/4
       bg-gradient-to-r from-spaceP via-amber-300 via-white via-cyan-100 to-spaceB
       animate-fade-right animate-once animate-delay-[2000ms]'>
