743dc9af6224a020c134abdb504ef7ed98a8d474
hluuy <dudwns1289@gmail.com>
2024-01-22 09:42:26 +0900

    Fix: Signup & Nickname error logic

--- frontend/src/pages/Auth/CreateNickname.js
+++ frontend/src/pages/Auth/CreateNickname.js
@@ -6,11 +6,11 @@ const CreateNickname = () => {
     const onClick = (path) => navigate(`/${path}`);
 
     const [form, setForm] = useState({
-        nickname: "",
+        userNickname: "",
     });
 
     const [err, setErr] = useState({
-        nickname: "",
+        userNickname: "",
     });
 
     const onChange = (e) => {
@@ -21,16 +21,61 @@ const CreateNickname = () => {
 
     const onSubmit = async (e) => {
         e.preventDefault();
+
+        let newErr = { ...err };
         // 입력 값 범위 설정
         const eRegEx = /^[a-z0-9A-Z가-힣ㄱ-ㅎ]{2,10}$/;
 
-        if (!eRegEx.test(form.nickname)) {
-            setErr((err) => ({ ...err, nickname: "한글, 영어, 숫자만 써주세요" }));
+        // 닉네임 검사
+        // 닉네임 input에 입력하지 않았다면
+        if (!form.userNickname) {
+            newErr.userNickname = "닉네임을 입력해주세요";
+            // 입력 기준을 충족하지 못했다면
+        } else if (!eRegEx.test(form.userNickname)) {
+            newErr.userNickname = "한글, 영어, 숫자만 써주세요 (2-10자)";
+            // 모든 기준 충족 시, 에러메시지 초기화
+        } else {
+            newErr.userNickname = "";
         }
-        if (form.nickname.length < 2 || form.nickname.length > 10) {
-            setErr((err) => ({ ...err, nickname: "2-10글자 사이로 써주세요" }));
+
+        // 에러 상태 업데이트
+        setErr(newErr);
+
+        if (newErr.userNickname === "") {
+            console.log("닉네임 :", form);
         }
     };
+
+    // const CreateNickname = () => {
+    //     const navigate = useNavigate();
+    //     const onClick = (path) => navigate(`/${path}`);
+
+    //     const [form, setForm] = useState({
+    //         nickname: "",
+    //     });
+
+    //     const [err, setErr] = useState({
+    //         nickname: "",
+    //     });
+
+    //     const onChange = (e) => {
+    //         const { name, value } = e.currentTarget;
+    //         setForm({ ...form, [name]: value });
+    //         setErr({ ...err, [name]: "" });
+    //     };
+
+    //     const onSubmit = async (e) => {
+    //         e.preventDefault();
+    //         // 입력 값 범위 설정
+    //         const eRegEx = /^[a-z0-9A-Z가-힣ㄱ-ㅎ]{2,10}$/;
+
+    //         if (!eRegEx.test(form.nickname)) {
+    //             setErr((err) => ({ ...err, nickname: "한글, 영어, 숫자만 써주세요" }));
+    //         }
+    //         if (form.nickname.length < 2 || form.nickname.length > 10) {
+    //             setErr((err) => ({ ...err, nickname: "2-10글자 사이로 써주세요" }));
+    //         }
+    //     };
     return (
         <div className="w-full h-screen p-5 flex justify-center items-center z-10">
             <form
@@ -41,8 +86,14 @@ const CreateNickname = () => {
                 <h1 className="font-['pixel'] text-5xl">닉네임 생성</h1>
 
                 {/* 닉네임 입력창 */}
-                <input type="text" placeholder="닉네임" onChange={onChange} name="nickname" value={form.nickname} />
-                <p className="font-['pixel'] text-red-500 mb-1">{err.nickname}</p>
+                <input
+                    type="text"
+                    placeholder="닉네임"
+                    onChange={onChange}
+                    name="userNickname"
+                    value={form.userNickname}
+                />
+                <p className="font-['pixel'] text-red-500 mb-1">{err.userNickname}</p>
 
                 <button className="font-['pixel'] p-2 m-1 rounded w-72 bg-formButton">입장하기</button>
             </form>
--- frontend/src/pages/Auth/Signup.js
+++ frontend/src/pages/Auth/Signup.js
@@ -15,6 +15,7 @@ const Signup = () => {
         userPassword: "",
         passwordCheck: "",
     });
+
     const [showPassword, setShowPassword] = useState(false);
 
     const onChange = (e) => {
@@ -27,50 +28,143 @@ const Signup = () => {
         e.preventDefault();
         setShowPassword((showPassword) => !showPassword);
     };
+
     const onSubmit = async (e) => {
         e.preventDefault();
-        // 입력 값 범위 설정
+
+        // 새로운 에러 객체 생성
+        let newErr = { ...err };
+        // 아이디, 비밀번호 입력 기준 설정
         const eRegEx = /^[a-z0-9A-Z]{4,20}$/;
-        // 아이디 입력 안했을 때
+
+        // 아이디 검사
+        // 아이디 input에 입력하지 않았다면
         if (!form.userId) {
-            setErr((err) => ({ ...err, userId: "아이디를 입력해주세요" }));
-            // 영어와 숫자를 제외한 것을 입력했을 때
+            newErr.userId = "아이디를 입력해주세요";
+            // 입력 기준 충족하지 못한다면
         } else if (!eRegEx.test(form.userId)) {
-            setErr((err) => ({ ...err, userId: "영어랑 숫자만 써주세요" }));
+            newErr.userId = "영어, 숫자만 써주세요 (4-20자)";
+        } else {
+            newErr.userId = "";
         }
-        // 비밀번호 입력 안했을 때
+        // eRegEx에서 글자 수 제한이 정해져 있지만 에러메시지 분기점을 위해
+        // 글자 수 넘어가면 에러메시지 뜨도록
+        // if (form.userId.length > 20){
+        //     newErr.userId = "아이디는 4자 이상으로 입력해주세요";
+        // } else if (form.userId.length < 4) {
+        //     newErr.userId = "아이디는 20자 이하로 해주세요";
+        // } else {
+        //     newErr.userId = "";
+        // }
+
+        // 비밀번호 검사
+        // 비밀번호 input에 입력하지 않았다면
         if (!form.userPassword) {
-            setErr((err) => ({ ...err, userPassword: "비밀번호를 입력해주세요" }));
-            // 영어와 숫자를 제외한 것을 입력했을 때
+            newErr.userPassword = "비밀번호를 입력해주세요";
+            // 입력 기준 충족하지 못한다면
         } else if (!eRegEx.test(form.userPassword)) {
-            setErr((err) => ({ ...err, userPassword: "영어랑 숫자만 써주세요" }));
+            newErr.userPassword = "영어랑 숫자만 써주세요 (4-20자)";
+        } else {
+            newErr.userPassword = "";
         }
-        // 비밀번호가 4자리 미만일 때
-        if (form.userPassword.length > 0 && form.userPassword.length < 4) {
-            setErr((err) => ({ ...err, userPassword: "비밀번호는 4자 이상으로 입력해주세요" }));
-        }
-        // 비밀번호 길이가 20자가 넘어갔을 때
-        if (form.userPassword.length > 20) {
-            setErr((err) => ({ ...err, userPassword: "비밀번호는 20자 이하로 입력해주세요" }));
-        }
-        // 비밀번호 확인 입력 안했을 때
+        // eRegEx에서 글자 수 제한이 정해져 있지만 에러메시지 분기점을 위해
+        // 글자 수 넘어가면 에러메시지 뜨도록
+        // if (form.userPassword.length < 4) {
+        //     newErr.userPassword = "비밀번호는 4자 이상으로 입력해주세요";
+        // } else if (form.userPassword.length > 20) {
+        //     newErr.userPassword = "비밀번호는 20자 이하로 입력해주세요";
+        // } else {
+        //     newErr.userPassword = "";
+        // }
+
+        // 비밀번호 확인 검사
         if (!form.passwordCheck) {
-            setErr((err) => ({ ...err, passwordCheck: "비밀번호를 입력해주세요" }));
+            newErr.passwordCheck = "비밀번호를 입력해주세요";
+        } else if (form.userPassword !== form.passwordCheck) {
+            newErr.passwordCheck = "비밀번호가 일치하지 않습니다";
+        } else {
+            newErr.passwordCheck = "";
         }
-        // 비밀번호와 비밀번호 확인이 일치하지 않을 때
-        if (form.userPassword !== form.passwordCheck) {
-            setErr((err) => ({ ...err, passwordCheck: "비밀번호가 일치하지 않습니다" }));
-        }
-        // 비밀번호와 비밀번호 확인이 일치하면 에러메시지 지우기
-        if (form.userPassword === form.passwordCheck) {
-            setErr((err) => ({ ...err, passwordCheck: "" }));
-        }
-        // 모든 유효성 검사를 통과했다고 가정
-        if (!err.userId && !err.userPassword && !err.passwordCheck && form.userPassword === form.passwordCheck) {
-            // 콘솔창에 사용자 입력 값 출력
+
+        // 에러 상태 업데이트
+        setErr(newErr);
+
+        // 모든 유효성 검사를 통과하면 서버로 데이터 전송
+        if (newErr.userId === "" && newErr.userPassword === "" && newErr.passwordCheck === "") {
             console.log("회원가입 정보:", form);
         }
     };
+
+// const Signup = () => {
+//     const navigate = useNavigate();
+//     const onClick = (path) => navigate(`/${path}`);
+
+//     const [form, setForm] = useState({
+//         userId: "",
+//         userPassword: "",
+//         passwordCheck: "",
+//     });
+//     const [err, setErr] = useState({
+//         userId: "",
+//         userPassword: "",
+//         passwordCheck: "",
+//     });
+//     const [showPassword, setShowPassword] = useState(false);
+
+//     const onChange = (e) => {
+//         const { name, value } = e.currentTarget;
+//         setForm({ ...form, [name]: value });
+//         setErr({ ...err, [name]: "" });
+//     };
+//     // 버튼 클릭 시, 비밀번호 입력창에서 text와 "*" 사이를 전환
+//     const togglePassword = (e) => {
+//         e.preventDefault();
+//         setShowPassword((showPassword) => !showPassword);
+//     };
+//     const onSubmit = async (e) => {
+//         e.preventDefault();
+//         // 입력 값 범위 설정
+//         const eRegEx = /^[a-z0-9A-Z]{4,20}$/;
+//         // 아이디 입력 안했을 때
+//         if (!form.userId) {
+//             setErr((err) => ({ ...err, userId: "아이디를 입력해주세요" }));
+//             // 영어와 숫자를 제외한 것을 입력했을 때
+//         } else if (!eRegEx.test(form.userId)) {
+//             setErr((err) => ({ ...err, userId: "영어랑 숫자만 써주세요" }));
+//         }
+//         // 비밀번호 입력 안했을 때
+//         if (!form.userPassword) {
+//             setErr((err) => ({ ...err, userPassword: "비밀번호를 입력해주세요" }));
+//             // 영어와 숫자를 제외한 것을 입력했을 때
+//         } else if (!eRegEx.test(form.userPassword)) {
+//             setErr((err) => ({ ...err, userPassword: "영어랑 숫자만 써주세요" }));
+//         }
+//         // 비밀번호가 4자리 미만일 때
+//         if (form.userPassword.length > 0 && form.userPassword.length < 4) {
+//             setErr((err) => ({ ...err, userPassword: "비밀번호는 4자 이상으로 입력해주세요" }));
+//         }
+//         // 비밀번호 길이가 20자가 넘어갔을 때
+//         if (form.userPassword.length > 20) {
+//             setErr((err) => ({ ...err, userPassword: "비밀번호는 20자 이하로 입력해주세요" }));
+//         }
+//         // 비밀번호 확인 입력 안했을 때
+//         if (!form.passwordCheck) {
+//             setErr((err) => ({ ...err, passwordCheck: "비밀번호를 입력해주세요" }));
+//         }
+//         // 비밀번호와 비밀번호 확인이 일치하지 않을 때
+//         if (form.userPassword !== form.passwordCheck) {
+//             setErr((err) => ({ ...err, passwordCheck: "비밀번호가 일치하지 않습니다" }));
+//         }
+//         // 비밀번호와 비밀번호 확인이 일치하면 에러메시지 지우기
+//         if (form.userPassword === form.passwordCheck) {
+//             setErr((err) => ({ ...err, passwordCheck: "" }));
+//         }
+//         // 모든 유효성 검사를 통과했다고 가정
+//         if (!err.userId && !err.userPassword && !err.passwordCheck && form.userPassword === form.passwordCheck) {
+//             // 콘솔창에 사용자 입력 값 출력
+//             console.log("회원가입 정보:", form);
+//         }
+//     };
     return (
         <div className="w-full h-screen p-5 flex justify-center items-center z-10">
             <form
