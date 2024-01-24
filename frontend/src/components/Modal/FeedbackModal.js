13de1491a6096a6436a13e8e125946efa29acb30
hluuy <dudwns1289@gmail.com>
2024-01-24 21:28:41 +0900

    Feat: Send feedback message

--- frontend/src/components/Modal/FeedbackModal.js
+++ frontend/src/components/Modal/FeedbackModal.js
@@ -1,4 +1,27 @@
+import React, { useState } from "react";
+import axios from "axios";
+
 const FeedbackModal = (props) => {
+  const [feedback, setFeedback] = useState("");
+  const userNickname = sessionStorage.getItem("userNickname");
+
+  const onChange = (e) => {
+    setFeedback(e.currentTarget.value);
+  };
+  // sendFeedback console 버전
+  const sendFeedbackConsole = () => {
+    console.log({userNickname, feedback});
+    props.setFeedback(false);
+  }
+  // 피드백 내용을 가지고 axios 요청
+  const sendFeedback = async () =>{
+    try {
+      await axios.post("http://localhost:5000/feedback", { feedback });
+      props.setFeedback(false);
+    } catch (error) {
+      console.error("feedback 전송 실패", error);
+    }
+  };
   return (
     <>
       <div
@@ -13,9 +36,11 @@ const FeedbackModal = (props) => {
                 placeholder="피드백을 입력해주세요!"
                 className="m-1 px-2 
           border rounded-3xl bg-white"
+                value={feedback}
+                onChange={onChange}
               />
             </div>
-            <button onClick={props.setFeedback} className="bg-formButton py-2 px-4 m-2 rounded">
+            <button onClick={sendFeedbackConsole} className="bg-formButton py-2 px-4 m-2 rounded">
               보내기
             </button>
           </div>
