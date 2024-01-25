4b6595acaf7c6d0073d1f14c4e86b8ed3e7fcd6b
황채원 <5959.wondreaming@gmail.com>
2024-01-25 12:40:43 +0900

    Feat: joinRoom

--- frontend/src/components/lobbyComponent/RoomList.js
+++ frontend/src/components/lobbyComponent/RoomList.js
@@ -1,15 +1,13 @@
-import { OpenVidu } from "openvidu-browser";
-
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Rooms from "./Rooms";
 import UseRoomListApiCall from "../../api/UseRoomListApiCall";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 // 방목록 조회 컴포넌트
 const RoomList = () => {
   // 방 리스트를 받는 변수
   const [rooms, setRooms] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     UseRoomListApiCall()
@@ -53,64 +51,13 @@ const RoomList = () => {
     };
   }, [rooms]); // rooms가 변경될 때마다 실행
 
-  // 룸 입장시, token 발행
-  const createToken = async (sessionId) => {
-    const response = await axios.post(
-      "http://localhost:5000/" + "tokens/" + sessionId,
-      {},
-      {
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-    console.log(response.data);
-    return response.data; // The token
-  };
-
-  // 룸에 join하기
-  const OV = useRef(new OpenVidu());
-  const [subscribers, setSubscribers] = useState([]);
-  const [session, setSession] = useState(undefined);
-  const navigate = useNavigate();
-
-  const joinSession = useCallback(
-    async (token, roomId) => {
-      const mySession = OV.current.initSession();
-
-      mySession.on("streamCreated", (event) => {
-        const subscriber = mySession.subscribe(event.stream, undefined);
-        setSubscribers((subscribers) => [...subscribers, subscriber]);
-      });
-
-      mySession.on("exception", (exception) => {
-        console.warn(exception);
-      });
-
-      try {
-        await mySession.connect(token);
-        console.log("방 입장!");
-        setSession(mySession);
-        navigate(`/room`);
-      } catch (error) {
-        console.error("방 입장 못해용", error);
-      }
-
-      setSession(mySession);
-    },
-    [navigate]
-  );
-
   return (
     <section className="p-3 border-7 border-modalBorder mt-4 col-start-2 col-end-7 row-start-2 row-end-13">
       <div className="flex flex-wrap mx-2 overflow-y-scroll h-[69vh]">
         {rooms.map((room, i) => (
           <div
-            onClick={async () => {
-              try {
-                const token = await createToken(room.sessionId);
-                await joinSession(token, room.roomId);
-              } catch (error) {
-                console.error("방 입장 못해용", error);
-              }
+            onClick={() => {
+              navigate(`/room/${room.sessionId}`);
             }}
             className="w-1/2"
             ref={(el) => (roomRefs.current[i] = el)}
