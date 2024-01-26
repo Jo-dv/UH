7f341ae751d53fb917963471fcb02a4522c8f625
Shinhyeonjung <tlswnd3127@gmail.com>
2024-01-26 12:47:06 +0900

    Fix merge conflict

--- frontend/src/pages/Auth/CreateNickname.js
+++ frontend/src/pages/Auth/CreateNickname.js
@@ -3,140 +3,117 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const CreateNickname = () => {
-    const navigate = useNavigate();
-    const onClick = (path) => navigate(`/${path}`);
+  const navigate = useNavigate();
+  const onClick = (path) => navigate(`/${path}`);
 
-    const [form, setForm] = useState({
-        userNickname: "",
-    });
+  const [form, setForm] = useState({
+    userNickname: "",
+  });
 
-    const [err, setErr] = useState({
-        userNickname: "",
-    });
+  const [err, setErr] = useState({
+    userNickname: "",
+  });
 
-    const [checkUserNickname, setCheckUserNickname] = useState("");
+  const [checkUserNickname, setCheckUserNickname] = useState("");
 
-    const onChange = (e) => {
-        const { name, value } = e.currentTarget;
-        setForm({ ...form, [name]: value });
-        setErr({ ...err, [name]: "" });
-    };
+  const onChange = (e) => {
+    const { name, value } = e.currentTarget;
+    setForm({ ...form, [name]: value });
+    setErr({ ...err, [name]: "" });
+  };
 
-    // const checkNickname = async () => {
-    //     console.log("ㄱㄱ")
-    //     try {
-    //         const response = await axios.post("/user/nickname-check", {
-    //             userNickname: form.userNickname,
-    //         });
-    //         const res = response.data;
-    //         console.log(res);
-    //         if (res === "가능") {
-    //             setCheckUserNickname("사용가능한 닉네임");
-    //         } else {
-    //             setCheckUserNickname("중복된 닉네임");
-    //         }
-    //     } catch (error) {
-    //         console.error("에러 발생", error);
-    //     }
+  // const checkNickname = async () => {
+  //     console.log("ㄱㄱ")
+  //     try {
+  //         const response = await axios.post("/user/nickname-check", {
+  //             userNickname: form.userNickname,
+  //         });
+  //         const res = response.data;
+  //         console.log(res);
+  //         if (res === "가능") {
+  //             setCheckUserNickname("사용가능한 닉네임");
+  //         } else {
+  //             setCheckUserNickname("중복된 닉네임");
+  //         }
+  //     } catch (error) {
+  //         console.error("에러 발생", error);
+  //     }
 
-    // };
+  // };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-        let newErr = { ...err };
-        // 입력 값 범위 설정
-        const eRegEx = /^[a-z0-9A-Z가-힣ㄱ-ㅎ]{2,10}$/;
+    let newErr = { ...err };
+    // 입력 값 범위 설정
+    const eRegEx = /^[a-z0-9A-Z가-힣ㄱ-ㅎ]{2,10}$/;
 
-        // 닉네임 검사
-        // 닉네임 input에 입력하지 않았다면
-        if (!form.userNickname) {
-            newErr.userNickname = "닉네임을 입력해주세요";
-            // 입력 기준을 충족하지 못했다면
-        } else if (!eRegEx.test(form.userNickname)) {
-            newErr.userNickname = "한글, 영어, 숫자만 써주세요 (2-10자)";
-            // 닉네임 중복 검사
-<<<<<<< HEAD
-        // } else if (checkUserNickname !== "사용가능한 닉네임") {
-        //     newErr.userNickname = "중복된 닉네임입니다";
-=======
-            // } else if (checkUserNickname !== "사용가능한 닉네임") {
-            //     newErr.userNickname = "중복된 닉네임입니다";
->>>>>>> fe_dev
-            // 모든 기준 충족 시, 에러메시지 초기화
+    // 닉네임 검사
+    // 닉네임 input에 입력하지 않았다면
+    if (!form.userNickname) {
+      newErr.userNickname = "닉네임을 입력해주세요";
+      // 입력 기준을 충족하지 못했다면
+    } else if (!eRegEx.test(form.userNickname)) {
+      newErr.userNickname = "한글, 영어, 숫자만 써주세요 (2-10자)";
+      // 닉네임 중복 검사
+      // } else if (checkUserNickname !== "사용가능한 닉네임") {
+      //     newErr.userNickname = "중복된 닉네임입니다";
+      // 모든 기준 충족 시, 에러메시지 초기화
+    } else {
+      newErr.userNickname = "";
+    }
+
+    // 에러 상태 업데이트
+    setErr(newErr);
+
+    if (newErr.userNickname === "") {
+      console.log("닉네임 :", form);
+      const userSeq = sessionStorage.getItem("userSeq");
+      try {
+        const response = await axios.post("http://localhost:5000/user/nickname", {
+          userSeq,
+          userNickname: form.userNickname,
+        });
+        const res = response.data;
+        console.log("서버 응답:", res);
+        if (res.status === 400) {
+          setErr({ ...err, userNickname: "중복된 닉네임입니다" });
         } else {
-            newErr.userNickname = "";
+          sessionStorage.setItem("userNickname", form.userNickname);
+          navigate("/lobby");
         }
+      } catch (error) {
+        console.error("닉네임 생성 중 에러 발생", error);
+        // 에러 처리
+        // 예: 사용자에게 에러 메시지 표시
+      }
+    }
+  };
 
-        // 에러 상태 업데이트
-        setErr(newErr);
-
-        if (newErr.userNickname === "") {
-            console.log("닉네임 :", form);
-<<<<<<< HEAD
-            const userId = sessionStorage.getItem("userId");
-            try {
-                const response = await axios.post("http://localhost:5000/user/nickname", {
-                    userId,
-                    userNickname: form.userNickname,
-                });
-                const res = response.data;
-                console.log("서버 응답:", res)
-                if ( res.status === 400 ) {
-                    setErr({ ...err, userNickname: "중복된 닉네임입니다" });
-                } else {
-                navigate("/lobby");
-            }
-=======
-            const userSeq = sessionStorage.getItem("userSeq");
-            try {
-                const response = await axios.post("http://localhost:5000/user/nickname", {
-                    userSeq,
-                    userNickname: form.userNickname,
-                });
-                const res = response.data;
-                console.log("서버 응답:", res);
-                if (res.status === 400) {
-                    setErr({ ...err, userNickname: "중복된 닉네임입니다" });
-                } else {
-                    sessionStorage.setItem("userNickname", form.userNickname);
-                    navigate("/lobby");
-                }
->>>>>>> fe_dev
-            } catch (error) {
-                console.error("닉네임 생성 중 에러 발생", error);
-                // 에러 처리
-                // 예: 사용자에게 에러 메시지 표시
-            }
-        }
-    };
-
-    return (
-        <div className="w-full h-screen p-5 flex justify-center items-center z-10">
-            <form
-                onSubmit={onSubmit}
-                className="bg-opacity-50 bg-formBG w-96 border-2 border-purple3
+  return (
+    <div className="w-full h-screen p-5 flex justify-center items-center z-10">
+      <form
+        onSubmit={onSubmit}
+        className="bg-opacity-50 bg-formBG w-96 border-2 border-purple3
         flex flex-col justify-center items-center z-20"
-            >
-                <h1 className="font-['pixel'] text-5xl">닉네임 생성</h1>
+      >
+        <h1 className="font-['pixel'] text-5xl">닉네임 생성</h1>
 
-                {/* 닉네임 입력창 */}
-                <input
-                    type="text"
-                    placeholder="닉네임"
-                    onChange={onChange}
-                    // onBlur={checkNickname}
-                    name="userNickname"
-                    value={form.userNickname}
-                />
-                <p className="font-['pixel'] text-red-500 mb-1">{err.userNickname}</p>
-                <span>{checkUserNickname}</span>
+        {/* 닉네임 입력창 */}
+        <input
+          type="text"
+          placeholder="닉네임"
+          onChange={onChange}
+          // onBlur={checkNickname}
+          name="userNickname"
+          value={form.userNickname}
+        />
+        <p className="font-['pixel'] text-red-500 mb-1">{err.userNickname}</p>
+        <span>{checkUserNickname}</span>
 
-                <button className="font-['pixel'] p-2 m-1 rounded w-72 bg-formButton">
-                    입장하기
-                </button>
-            </form>
-        </div>
-    );
+        <button className="font-['pixel'] p-2 m-1 rounded w-72 bg-formButton">입장하기</button>
+      </form>
+    </div>
+  );
 };
 export default CreateNickname;
--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -7,140 +7,140 @@ import kakaoLogo from "./img/kakaoLogoB.png";
 import naverLogo from "./img/naverLogo.png";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    // 카카오 소셜 로그인 
-    const REST_API_KEY = '4fffa78521feee5e1eb947c704c08cf2';
-    const REDIRECT_URI = 'http://localhost:3000/callback/kakao';
-    const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-    const kakaoLoginHandler = () => {
-        window.location.href = link;
-    };
+  // 카카오 소셜 로그인
+  const REST_API_KEY = "4fffa78521feee5e1eb947c704c08cf2";
+  const REDIRECT_URI = "http://localhost:3000/callback/kakao";
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const kakaoLoginHandler = () => {
+    window.location.href = link;
+  };
 
-    const [form, setForm] = useState({
-        userId: "",
-        userPassword: "",
-    });
-    const [err, setErr] = useState({
-        userId: "",
-        userPassword: "",
-    });
-    const onChange = (e) => {
-        const { name, value } = e.currentTarget;
-        setForm({ ...form, [name]: value });
-    };
+  const [form, setForm] = useState({
+    userId: "",
+    userPassword: "",
+  });
+  const [err, setErr] = useState({
+    userId: "",
+    userPassword: "",
+  });
+  const onChange = (e) => {
+    const { name, value } = e.currentTarget;
+    setForm({ ...form, [name]: value });
+  };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-        let newErr = { ...err };
-        if ( !form.userId ) {
-            newErr.userId = "아이디를 입력해주세요";
+    let newErr = { ...err };
+    if (!form.userId) {
+      newErr.userId = "아이디를 입력해주세요";
+    } else {
+      newErr.userId = "";
+    }
+    if (!form.userPassword) {
+      newErr.userPassword = "비밀번호를 입력해주세요";
+    } else {
+      newErr.userPassword = "";
+    }
+    // if (!form.userId && !form.password) {
+    //     return setErr({
+    //         userId: "아이디를 입력해주세요",
+    //         password: "비밀번호를 입력해주세요",
+    //     });
+    // }
+    // if (!form.userId) {
+    //     setErr({ ...err, userId: "아이디를 입력해주세요", password: "" });
+    // }
+    // if (!form.password) {
+    //     setErr({
+    //         ...err,
+    //         userId: "",
+    //         password: "비밀번호를 입력해주세요",
+    //     });
+    // }
+    // if (!!form.userId && !!form.password) {
+    //     return setErr({
+    //         userId: "",
+    //         password: "",
+    //     });
+    // }
+    setErr(newErr);
+
+    if (newErr.userId === "" && newErr.userPassword === "") {
+      const { userId, userPassword } = form;
+      console.log("로그인 정보 :", { userId, userPassword });
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/user/login",
+          { userId, userPassword },
+          { withCredentials: true }
+        );
+        const res = response.data;
+        console.log("서버 응답:", res);
+        if (res.userNickname === null) {
+          sessionStorage.setItem("userSeq", res.userSeq);
+          navigate("/auth/nickname");
         } else {
-            newErr.userId = "";
+          sessionStorage.setItem("userNickname", res.userNickname);
+          sessionStorage.setItem("userSeq", res.userSeq);
+          console.log("로그인 성공");
+          navigate("/lobby");
         }
-        if (!form.userPassword ) {
-            newErr.userPassword = "비밀번호를 입력해주세요";
-        } else {
-            newErr.userPassword = "";
-        }
-        // if (!form.userId && !form.password) {
-        //     return setErr({
-        //         userId: "아이디를 입력해주세요",
-        //         password: "비밀번호를 입력해주세요",
-        //     });
-        // }
-        // if (!form.userId) {
-        //     setErr({ ...err, userId: "아이디를 입력해주세요", password: "" });
-        // }
-        // if (!form.password) {
-        //     setErr({
-        //         ...err,
-        //         userId: "",
-        //         password: "비밀번호를 입력해주세요",
-        //     });
-        // }
-        // if (!!form.userId && !!form.password) {
-        //     return setErr({
-        //         userId: "",
-        //         password: "",
-        //     });
-        // }
-        setErr(newErr);
-
-        if (newErr.userId === "" && newErr.userPassword === "") {
-            const { userId, userPassword } = form;
-            console.log("로그인 정보 :", { userId, userPassword });
-            try {
-                const response = await axios.post("http://localhost:5000/user/login", { userId, userPassword },{ withCredentials: true });
-                const res = response.data
-                console.log("서버 응답:", res);
-                if (res.userNickname === null) {
-<<<<<<< HEAD
-                    sessionStorage.setItem("userId", userId);
-=======
-                    sessionStorage.setItem("userSeq", res.userSeq);
->>>>>>> fe_dev
-                    navigate("/auth/nickname");
-                } else {
-                    sessionStorage.setItem("userNickname", res.userNickname);
-                    sessionStorage.setItem("userSeq", res.userSeq);
-                    console.log("로그인 성공")
-                    navigate("/lobby");
-                }
-            } catch (error) {
-                console.error("로그인 중 에러 발생", error);
-                // 에러 처리
-                // 예: 사용자에게 에러 메시지 표시
-            }
-        }
-    };
-    return (
-        <div
-            className="w-full h-screen p-5 flex justify-center items-center z-10"
-        >
-            <form
-                onSubmit={onSubmit}
-                className="bg-opacity-50 bg-formBG w-96 border-2 border-purple3
+      } catch (error) {
+        console.error("로그인 중 에러 발생", error);
+        // 에러 처리
+        // 예: 사용자에게 에러 메시지 표시
+      }
+    }
+  };
+  return (
+    <div className="w-full h-screen p-5 flex justify-center items-center z-10">
+      <form
+        onSubmit={onSubmit}
+        className="bg-opacity-50 bg-formBG w-96 border-2 border-purple3
                 flex flex-col justify-center items-center z-20"
-            >
-                <h2 className="font-['pixel'] text-7xl">로그인</h2>
-                <input
-                    type="text"
-                    placeholder="아이디"
-                    onChange={onChange}
-                    name="userId"
-                    value={form.userId}
-                    className="font-['pixel'] p-2 m-1 border-2 rounded border-purple5 bg-input w-72"
-                />
-                <p className="font-['pixel'] text-red-500 mb-1">{err.userId}</p>
-                {/* {!err.userId && <p>{err.userId}</p>} */}
+      >
+        <h2 className="font-['pixel'] text-7xl">로그인</h2>
+        <input
+          type="text"
+          placeholder="아이디"
+          onChange={onChange}
+          name="userId"
+          value={form.userId}
+          className="font-['pixel'] p-2 m-1 border-2 rounded border-purple5 bg-input w-72"
+        />
+        <p className="font-['pixel'] text-red-500 mb-1">{err.userId}</p>
+        {/* {!err.userId && <p>{err.userId}</p>} */}
 
-                <input
-                    type="password"
-                    placeholder="비밀번호"
-                    onChange={onChange}
-                    name="userPassword"
-                    value={form.userPassword}
-                    className="font-['pixel'] p-2 m-1 border-2 rounded border-purple5 bg-input w-72"
-                />
-                <p className="font-['pixel'] text-red-500 mb-1">{err.userPassword}</p>
+        <input
+          type="password"
+          placeholder="비밀번호"
+          onChange={onChange}
+          name="userPassword"
+          value={form.userPassword}
+          className="font-['pixel'] p-2 m-1 border-2 rounded border-purple5 bg-input w-72"
+        />
+        <p className="font-['pixel'] text-red-500 mb-1">{err.userPassword}</p>
 
-                <button className="font-['pixel'] p-2 m-1 rounded w-72 bg-formButton">로그인</button>
-                <h3 className="p-2 m-2">
-                    <Link to="/auth/signup">회원가입</Link>
-                </h3>
-                <h3 className="p-2 ">소셜로그인</h3>
+        <button className="font-['pixel'] p-2 m-1 rounded w-72 bg-formButton">로그인</button>
+        <h3 className="p-2 m-2">
+          <Link to="/auth/signup">회원가입</Link>
+        </h3>
+        <h3 className="p-2 ">소셜로그인</h3>
 
-                <div className="flex flex-row justify-around w-72">
-                    <img src={googleLogo} alt="google Logo" />
-                    <img src={kakaoLogo} alt="google Logo" />
-                    <img src={naverLogo} alt="google Logo" />
-                </div>
-                <button type='button' onClick={kakaoLoginHandler}>카카오 로그인</button>
-            </form>
+        <div className="flex flex-row justify-around w-72">
+          <img src={googleLogo} alt="google Logo" />
+          <img src={kakaoLogo} alt="google Logo" />
+          <img src={naverLogo} alt="google Logo" />
         </div>
-    );
+        <button type="button" onClick={kakaoLoginHandler}>
+          카카오 로그인
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
