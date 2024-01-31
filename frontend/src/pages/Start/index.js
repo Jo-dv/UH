6ca6267c58bcacaface76a31bad91f96e4a17901
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2024-01-31 23:28:37 +0900

    Chore : image path modify

--- frontend/src/pages/Start/index.js
+++ frontend/src/pages/Start/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import logoImg from "/frontend/src/asset/image/LOGO.png";
-import startBackImg from "/frontend/src/asset/image/startBackGround.png";
+import logoImg from "../../asset/image/LOGO.png";
+import startBackImg from "../../asset/image/startBackGround.png";
 function Start() {
   const navigate = useNavigate();
 
@@ -24,15 +24,15 @@ function Start() {
         <img
           className="w-4/5 max-w-2xl
       animate-jump-in animate-duration-[2000ms] z-10"
-          alt="Logo"
           src={logoImg}
+          alt="Logo"
         />
 
         <img
           className="animate-fade animate-delay-[2000ms] 
         absolute h-screen w-full"
-          alt="Background"
           src={startBackImg}
+          alt="Background"
         />
         {/* <div className='z-10 absolute w-3/4 h-3/4
       bg-gradient-to-r from-spaceP via-amber-300 via-white via-cyan-100 to-spaceB
