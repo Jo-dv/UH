c64b0b4093042d59873986c8e5a1b580d5c2a368
이성완 <cjddktkdwk@naver.com>
2024-01-31 12:10:22 +0900

    Refactor : gameDto,Dao 수정
    dto 사진 url 값 제거
    Dao limit 설정 rand 시드값 제거

--- backend/src/main/java/org/project/uh/game/dao/GameDao.java
+++ backend/src/main/java/org/project/uh/game/dao/GameDao.java
@@ -13,13 +13,13 @@ public interface GameDao {
 	@Select("select * "
 		+ "from quiz_shout "
 		+ "where quiz_category=#{quizCategory} "
-		+ "ORDER BY RAND(123)")
+		+ "ORDER BY RAND() limit 150")
 		// 무작위 문제 생성
 	List<QuizDto> shoutList(int quizCategory);
 
 	@Select("select * "
 		+ "from quiz_person "
-		+ "ORDER BY RAND(123)")
+		+ "ORDER BY RAND() limit 150")
 		// 무작위 문제 생성
 	List<QuizDto> personList();
 
