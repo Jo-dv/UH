52a80cb3625c7cb0d2487c06f688b45dab914790
이성완 <cjddktkdwk@naver.com>
2024-02-06 16:14:42 +0900

    Fix : 웹소켓 url 변경

--- backend/src/main/java/org/project/uh/websocket/WebSocketConfig.java
+++ backend/src/main/java/org/project/uh/websocket/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(webSocketHandler, "/ws")
+		registry.addHandler(webSocketHandler, "/api/ws")
 			.addInterceptors(handshakeInterceptor)
 			.setAllowedOrigins("*");
 	}
