07acb4b78f766599c30cb8ec4e171a0f954076b2
이성완 <cjddktkdwk@naver.com>
2024-02-06 16:19:30 +0900

    Fix : socket url 변경

--- frontend/src/webSocket/UseWebSocket.js
+++ frontend/src/webSocket/UseWebSocket.js
@@ -39,7 +39,7 @@ export const WebSocketProvider = ({ children }) => {
     };
 
     connect(
-      "wss://i10e201.p.ssafy.io/api/ws",
+      "ws://i10e201.p.ssafy.io/api/ws",
       null,
       (event) => {
         console.log("WebSocket 메시지 수신:", event.data);
