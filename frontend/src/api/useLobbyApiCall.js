69d6ae5309c64ae7e35c55a64542de4364af625f
박수빈 <bin577733@naver.com>
2024-02-13 15:14:26 +0900

    Update : useLobbyApiCall import 수정

--- frontend/src/api/useLobbyApiCall.js
+++ frontend/src/api/useLobbyApiCall.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axios from "./axios";
 import FriendAlready from "../components/Modal/Lobby/FriendAlready";
 
 const useLobbyApiCall = () => {
